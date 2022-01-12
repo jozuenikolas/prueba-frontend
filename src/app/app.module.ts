337d04697d72d0718b538daf1f5113a510a393c7
Nicolás Arias <josue.arias@epn.edu.ec>
2022-01-11 21:20:11 -0500

    Adds components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListPokemonsComponent } from './components/list-pokemons/list-pokemons.component';
+import { CreatePokemonComponent } from './components/create-pokemon/create-pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListPokemonsComponent,
+    CreatePokemonComponent
   ],
   imports: [
     BrowserModule,
